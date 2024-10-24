7cd87d89adc56a0f492ef2b80de85f470ab85bd8
Philip Ajuong Deng <philipajuong@gmail.com>
2024-10-24 12:05:49 +0000

    0x02. Redis basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,55 @@
 #!/usr/bin/env python3
-"""
-web cache and tracker
+"""Module for implementing an expiring web cache and tracker
 """
 import requests
-import redis
+import time
 from functools import wraps
 
-store = redis.Redis()
+CACHE_EXPIRATION_TIME = 10  # seconds
+CACHE = {}
 
 
-def count_url_access(method):
-    """ Decorator counting how many times
-    a URL is accessed """
-    @wraps(method)
-    def wrapper(url):
-        cached_key = "cached:" + url
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
+def cache(fn):
+    """_summary_
 
-        count_key = "count:" + url
-        html = method(url)
+    Args:
+        fn (function): _description_
 
-        store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
-        return html
-    return wrapper
+    Returns:
+        _type_: _description_
+    """
+    @wraps(fn)
+    def wrapped(*args, **kwargs):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        url = args[0]
+        if url in CACHE and CACHE[url]["timestamp"] + CACHE_EXPIRATION_TIME > \
+                time.time():
+            CACHE[url]["count"] += 1
+            return CACHE[url]["content"]
+        else:
+            content = fn(*args, **kwargs)
+            CACHE[url] = {"content": content,
+                          "timestamp": time.time(), "count": 1}
+            return content
+    return wrapped
 
 
-@count_url_access
+@cache
 def get_page(url: str) -> str:
-    """ Returns HTML content of a url """
-    res = requests.get(url)
-    return res.text
+    """_summary_
+
+    Args:
+        url (str): _description_
+
+    Returns:
+        str: _description_
+    """
+    global count
+    # increment count
+    count += 1
+    response = requests.get(url)
+    return response.content.decode('utf-8')
