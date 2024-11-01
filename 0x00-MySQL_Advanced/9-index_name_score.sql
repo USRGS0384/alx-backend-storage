35f3a03f48dff494f98817c1bc383efd40c63b71
Philip Ajuong Deng <philipajuong@gmail.com>
2024-11-01 18:36:11 +0000

    0x00. MySQL advanced

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,3 +1,4 @@
 -- Creates index on the table names using 
 -- first letter of name column and score column
-CREATE INDEX idx_name_first_score ON names (name(1), score);
+CREATE INDEX idx_name_first_score 
+ON names (name(1), score);
